9a0c074ab82314ddc8da8823447a79accfeecb58
skcna2 <skcna2@gmail.com>
2025-05-14 19:37:08 +0200

    Terminado

--- 02-tiposdatosyoperadores/tiposdedatos-operadores/src/main.ts
+++ 02-tiposdatosyoperadores/tiposdedatos-operadores/src/main.ts
@@ -4,5 +4,5 @@ const precio_total: number = 120;
 const precio_bebidas: number = 18;
 const usuarios: number = 6;
 
-const por_persona: number = (precio_total - precio_bebidas) / 6;
-console.log(`Deben pagar ${por_persona} por persona`);
+const por_persona: number = (precio_total - precio_bebidas) / usuarios;
+console.log(`Deben pagar ${por_persona}€ por persona`);
