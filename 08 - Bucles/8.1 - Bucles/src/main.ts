9b0648bfa7e90c85649c7e650bd43e22e10ba696
skcna2 <skcna2@gmail.com>
2025-09-29 15:58:49 +0200

    refactor(game): implementar función para obtener pacientes pediátricos menores de diez años

--- 08 - Bucles/8.1 - Bucles/src/main.ts
+++ 08 - Bucles/8.1 - Bucles/src/main.ts
@@ -14,18 +14,29 @@ const obtenPacientesAsignadosAPediatria = (pacientes: Pacientes[]): Pacientes[] 
 
 //console.log(obtenPacientesAsignadosAPediatria(pacientes));
 
-//const obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios = (//
-//pacientes: Pacientes[]
-//): Pacientes[] => {
-//  // Tu implementación aquí :)
-//}//;
+const obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios = (pacientes: Pacientes[]): Pacientes[] => {
+  // Tu implementación aquí :)
+  const listPacientes: Pacientes[] = [];
+  for (let paciente of pacientes) {
+    if (paciente.especialidad === "Pediatra" && paciente.edad < 10) {
+      listPacientes.push(paciente);
+    }
+  }
+
+  return listPacientes;
+};
 
 //Elemento Resultado
 const resultado = document.getElementById("resultado") as HTMLElement;
-const mostrarResultado = () => {
+const mostrarResultado1 = () => {
   resultado.textContent = JSON.stringify(obtenPacientesAsignadosAPediatria(pacientes));
 };
-
-//Elemento boton
+const mostrarResultado2 = () => {
+  resultado.textContent = JSON.stringify(obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes));
+};
+//Botoones
 const botonPed = document.getElementById("btn-paciente-pediatria");
-botonPed?.addEventListener("click", mostrarResultado);
+botonPed?.addEventListener("click", mostrarResultado1);
+
+const botonPedMenor = document.getElementById("btn-paciente-pediatria-menor");
+botonPedMenor?.addEventListener("click", mostrarResultado2);
